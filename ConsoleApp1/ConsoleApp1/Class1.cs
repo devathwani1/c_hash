cb33f7047702c52224e3f9623be1a88606a1b65b
dev <dev@gmail.com>
2025-07-07 08:30:29 +0530

    doing codd

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        string x;
+        int a;
+        int b;
+        int c;
+        public void get()
+        {
+            //ReadLine -> read whole line 
+            //Read -> only read a single char
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void put()
+        {
+            c = a + b;
+            Console.WriteLine(c.ToString());
+        }
+            
+    }
+}
